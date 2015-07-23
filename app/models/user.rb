72c0e8167b313476510ca8da8d80c4738abd8b99
David Gardetti <dgardetti@gmail.com>
2015-07-22 23:48:18 -0500

    added password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_many :tasks
   validates :name, presence: true
   validates :email, presence: true
+    
+  def encrypt_password(unencrypted_password)
+    self.password = BCrypt::Password.create(unencrypted_password)
+  end
+  
+  def correct_password?(attempted_password)
+    BCrypt::Password.new(self.password) == attempted_password
+  end  
+  
 end
